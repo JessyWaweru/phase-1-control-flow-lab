c5503e288ea65d5f0c0613ed41a499118418cbc3
jessywaweru <jessy.waweru@student.moringaschool.com>
2023-03-16 12:30:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(rideLength){
+  if(rideLength <= 400) {
+    return 'This one is on me!'}
+    else if(rideLength > 2000, rideLength < 2500){
+      return 'I will gladly take your thirty bucks.'}
+      else if (rideLength>2500){
+        return 'No can do.'
+      }
+  }
+
+  // Wruuite your code here!
+ /* let ride='190feet'
+  let rides='2501 feet'
+  let distance='2001 feet'
+
+ if (ride>= '400 feet') {
+    ride='This one is on me!'
+      return ride}
+    
+  
+   if (distance >'2000 feet') {
+    distance='I will gladly take your thirty bucks.'
+    return distance}
+    
+
+    if (rides > '2500 feet') {
+      rides='No can do.'
+      return rides}
+    
+}*/
+
+function ternaryCheckCity(city){
+  return (city==='NYC' ? 'Ok, sounds good.' : 'No go.')
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(charm){
+  switch (charm) {
+    case 'generous':
+      return 'Thank you so much.'
 
-function switchOnCharmFromTip(){
+    case 'not as generous':
+      return 'Thank you.'
+
+      default:
+        return 'Bye.'
+  }
+  
   // Write your code here!
 }
